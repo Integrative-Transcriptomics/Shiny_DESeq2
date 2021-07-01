b7d5b03a55b85cbbc56f6042913a9e8dd8aebc36
jonathanwaehrer <jonathan.waehrer@student.uni-tuebingen.de>
2021-07-01 15:07:49 +0200

    added list of bugs

--- ShinySeq_run.R
+++
@@ -1,7 +0,0 @@
-source("./Components/ShinySeq_Packages.R")
-source("./Components/ShinySeq_Functions.R")
-source("./Components/ShinySeq_UI.R")
-source("./Components/ShinySeq_Server.R")
-
-shinyApp(ui, server)
-
