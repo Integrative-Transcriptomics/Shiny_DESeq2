696e666bd6709a3a6d75867c13b463a1ebbeb01d
jonathanwaehrer <jonathan.waehrer@student.uni-tuebingen.de>
2021-07-15 15:53:39 +0200

    fixed bugfix

--- Components/ShinySeq_Functions.R
+++ Components/ShinySeq_Functions.R
@@ -35,9 +35,12 @@ sortThatData = function(rawCounts, infoData, gffData){
   
   row.names(infoData) = infoData[,1]                  # set row names of info data to QBiC Code so it can be sorted by column names of count data
   # row.names(rawCounts) = rawCounts[, 1]              # set row names of raw data to gene ID
-  rawCounts = rawCounts[,-(2:7)]                     
+  rawCounts = rawCounts[,-(2:7),]                     
+  
+  #print(c("Geneid", row.names(infoData)))
   #infoData = infoData[colnames(rawCounts)[-1],]       # sort info data according to column name occurence in the counts file. Not occuring names will be removed
   infoData = infoData[row.names(infoData) %in% colnames(rawCounts)[-1],]       # remove all samples from sample prep file which are not in counts
+  rawCounts = rawCounts[,c("Geneid", row.names(infoData))]
   
   # Change row names of raw counts to "locus_tag, gene name" (only tag, if gff-file has no corresponding gene): 
   names = gffData[gffData$locus_tag %in% rawCounts$Geneid & gffData$gbkey == "Gene",]$Name  # Match locus_tag of gff with Geneid and get gene names
@@ -59,7 +62,7 @@ sortThatData = function(rawCounts, infoData, gffData){
   merged_treatments = unite(treatments, "merged", sep = "_")
   merged_treatments = make.unique.2(merged_treatments$merged, sep = "_") # enumarate duplicates
   colnames(rawCounts)[-1] = merged_treatments
-  rawCounts = rawCounts[,c("Geneid", merged_treatments)]  # sort 
+  #rawCounts = rawCounts[,c("Geneid", merged_treatments)]  # sort 
   row.names(infoData) = merged_treatments
   
   return(list(rawCounts, infoData))     
