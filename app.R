3f8a48bfa13858c6f37378d9810e4d3aba7f9b98
jonathanwaehrer <jonathan.waehrer@student.uni-tuebingen.de>
2022-09-15 11:28:44 +0200

    increased maximum file size

--- app.R
+++ app.R
@@ -3,5 +3,6 @@ source("./Components/ShinySeq_Functions.R")
 source("./Components/ShinySeq_UI.R")
 source("./Components/ShinySeq_Server.R")
 
+options(shiny.maxRequestSize=200*1024**2) 
 shinyApp(ui, server)
 
